f99f8f8d13c590deb64d452b09dbdb5e7d867d7c
Ivan Krushinski <118225585+crusaderqueen@users.noreply.github.com>
2023-01-28 12:17:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import './style.css'
+// These are is included in the latest three package itself so installing is no longer necessary
 import * as THREE from '/node_modules/three'
 import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js'
